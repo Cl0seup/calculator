4cca7e400dae5e851eb4d3363ee6fc4d8e669a1f
Cl0seup <sravan.kumar1691@gmail.com>
2018-02-28 18:57:00 +0530

    cal

---
+++ basic.js
@@ -0,0 +1,27 @@
+function add() {
+	var n1 = parseFloat(document.getElementById("n1").value);
+	var n2 = parseFloat(document.getElementById("n2").value);
+	document.getElementById("result").value = n1 + n2;
+
+
+}
+
+function divide() {
+	var n1 = parseFloat(document.getElementById("n1").value);
+	var n2 = parseFloat(document.getElementById("n2").value);
+	document.getElementById("result").value = n1 / n2;
+
+}
+
+function multiply() {
+	var n1 = parseFloat(document.getElementById("n1").value);
+	var n2 = parseFloat(document.getElementById("n2").value);
+	document.getElementById("result").value = n1 * n2;
+
+}
+
+function sub() {
+	var n1 = parseFloat(document.getElementById("n1").value);
+	var n2 = parseFloat(document.getElementById("n2").value);
+	document.getElementById("result").value = n1 - n2;
+}
